71df1c7ef2e58556285865c07b8ba6ce931d4142
Robbie <crash123@gmail.com>
2011-11-12 01:59:52 +1300

    Cats

--- stuff/src/root/nested/hello_world.py
+++ stuff/src/root/nested/hello_world.py
@@ -4,7 +4,7 @@ Created on Nov 11, 2011
 @author: Robbie
 '''
 if __name__ == '__main__':
-    word = raw_input("Who do you go for? ")
+    word = raw_input("Who do you sex for? ")
 
 for letter in word:
     call = "Gimme a " + letter + "!"
