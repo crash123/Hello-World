0b4d3964932086993d100435f3bbba41a57ec902
Robbie <crash123@gmail.com>
2011-11-12 01:58:20 +1300

    Initial commit

---
+++ stuff/src/root/nested/example.py
@@ -0,0 +1,7 @@
+'''
+Created on Nov 11, 2011
+
+@author: Robbie
+'''
+if __name__ == '__main__':
+    print 2+2
---
+++ stuff/src/root/nested/hello_world.py
@@ -0,0 +1,17 @@
+'''
+Created on Nov 11, 2011
+
+@author: Robbie
+'''
+if __name__ == '__main__':
+    word = raw_input("Who do you go for? ")
+
+for letter in word:
+    call = "Gimme a " + letter + "!"
+    print call
+    print letter + "!"
+print "What does that spell?"
+print word + "2"
+   
+    
+    
